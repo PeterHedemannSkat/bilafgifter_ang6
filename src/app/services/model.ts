812982899ce6892b883f63e6280c262e8be134b2
PeterHedemannSkat <Peter.Hedemann@ufst.dk>
2021-01-26 12:20:14 +0100

    refactor: code formatting (prettier)

--- src/app/services/model.ts
+++ src/app/services/model.ts
@@ -1,33 +1,25 @@
-import { Injectable } from "@angular/core";
-import { general, periodOptions } from "../commonUtil/interfaces";
-import { PeriodsService } from "./createPeriods";
+import { Injectable } from "@angular/core"
+import { general, periodOptions } from "../commonUtil/interfaces"
+import { PeriodsService } from "./createPeriods"
 
 @Injectable()
 export class Model {
-  years = [2021, 2020, 2019, 2018, 2017];
-  year = 2021;
+  years = [2021, 2020, 2019, 2018, 2017]
+  year = 2021
   parameter: any[] = [
     {
       id: "type",
       fn: () => {
-        return true;
+        return true
       },
       value: "car",
       options: () => {
-        const options = [
-          "car",
-          "van",
-          "taxi",
-          "trailer",
-          "mc",
-          "camper",
-          "bus"
-        ];
+        const options = ["car", "van", "taxi", "trailer", "mc", "camper", "bus"]
         return {
           options: options,
           data: null,
           id: "type"
-        };
+        }
       }
     },
     {
@@ -42,18 +34,18 @@ export class Model {
             "bus",
             "trailer"
           ]),
-          lilleTruck = this.valueIs("weightTruck") === "lessThan12t";
+          lilleTruck = this.valueIs("weightTruck") === "lessThan12t"
 
-        return vehicles || lilleTruck;
+        return vehicles || lilleTruck
       },
       options: () => {
-        const options = ["benzin", "diesel"];
+        const options = ["benzin", "diesel"]
 
         return {
           options: options,
           data: null,
           id: "fuel"
-        };
+        }
       },
       value: "benzin"
     },
@@ -66,9 +58,9 @@ export class Model {
             "car",
             "veteranbil",
             "taxi"
-          ]);
+          ])
 
-        return isCar_or_Van && this.valueIs("fuel") === "diesel";
+        return isCar_or_Van && this.valueIs("fuel") === "diesel"
       },
       value: null,
       options: () => {
@@ -76,26 +68,26 @@ export class Model {
           options: ["ja", "nej"],
           data: null,
           id: "particle"
-        };
+        }
       }
     },
     {
       id: "period",
       fn: () => {
         const type = this.valueIs("type"),
-          isCar_or_Van = this.valuePropIsEither("type", ["van", "car", "taxi"]);
+          isCar_or_Van = this.valuePropIsEither("type", ["van", "car", "taxi"])
 
-        return isCar_or_Van;
+        return isCar_or_Van
       },
       value: "1",
       options: () => {
-        const type = this.valueIs("type");
+        const type = this.valueIs("type")
 
         return {
           options: null,
           data: this.periods.getPeriodOptions(type),
           id: "period"
-        };
+        }
       }
     },
     {
@@ -104,16 +96,16 @@ export class Model {
       fn: () => {
         const type = this.valueIs("type"),
           period = this.valueIs("period"),
-          isCar_or_Van = this.valuePropIsEither("type", ["van", "car", "taxi"]);
+          isCar_or_Van = this.valuePropIsEither("type", ["van", "car", "taxi"])
 
         if (isCar_or_Van && period !== null) {
           const values =
-            type === "car" || type === "taxi" ? [2, 4] : [2, 4, 6, 8];
+            type === "car" || type === "taxi" ? [2, 4] : [2, 4, 6, 8]
 
-          return values.indexOf(Number(period)) > -1;
+          return values.indexOf(Number(period)) > -1
         }
 
-        return false;
+        return false
       },
       value: null,
       options: () => {
@@ -121,20 +113,20 @@ export class Model {
           options: null,
           data: this.getSubPeriods(),
           id: "subperiod"
-        };
+        }
       }
     },
     {
       id: "valgfri",
       fn: () => {
         const period = this.valueIs("period"),
-          sub = this.valueIs("subperiod");
+          sub = this.valueIs("subperiod")
 
         return (
           period === "4" &&
           sub === "2" &&
           this.valuePropIsEither("type", ["car", "taxi"])
-        );
+        )
       },
       value: null,
       options: () => {
@@ -142,7 +134,7 @@ export class Model {
           options: ["ejerafgift", "vaegtafgift"],
           data: null,
           id: "valgfri"
-        };
+        }
       }
     },
     {
@@ -152,9 +144,9 @@ export class Model {
           periodIsCorrect =
             (this.valueIs("period") && Number(this.valueIs("period")) <= 7) ||
             (this.valueIs("period") === "8" &&
-              this.valueIs("subperiod") === "2");
+              this.valueIs("subperiod") === "2")
 
-        return isVan && periodIsCorrect;
+        return isVan && periodIsCorrect
       },
       value: null,
       options: () => {
@@ -162,7 +154,7 @@ export class Model {
           options: ["privat", "blandet", "erhverv"],
           data: null,
           id: "privatanvendelse"
-        };
+        }
       }
     },
     {
@@ -176,9 +168,9 @@ export class Model {
           periodIsCorrect =
             Number(this.valueIs("period")) < 4 ||
             (this.valueIs("period") === "4" &&
-              this.valueIs("subperiod") === "2");
+              this.valueIs("subperiod") === "2")
 
-        return isVan && periodIsCorrect && isNotErhverv;
+        return isVan && periodIsCorrect && isNotErhverv
       },
       value: null,
       options: () => {
@@ -186,13 +178,13 @@ export class Model {
           options: ["moreThan3t", "lessThan3t"],
           data: null,
           id: "vaegtVareBil"
-        };
+        }
       }
     },
     {
       id: "weightTruck",
       fn: () => {
-        return this.valueIs("type") === "truck";
+        return this.valueIs("type") === "truck"
       },
       value: null,
       options: () => {
@@ -200,13 +192,13 @@ export class Model {
           options: ["moreThan12t", "lessThan12t"],
           data: null,
           id: "weightTruck"
-        };
+        }
       }
     },
     {
       id: "typeLargeTruck",
       fn: () => {
-        return this.valueIs("weightTruck") === "moreThan12t";
+        return this.valueIs("weightTruck") === "moreThan12t"
       },
       value: null,
       options: () => {
@@ -214,7 +206,7 @@ export class Model {
           options: ["regTruck", "vogntog"],
           data: null,
           id: "typeLargeTruck"
-        };
+        }
       }
     },
     {
@@ -224,7 +216,7 @@ export class Model {
           this.valueIs("weightTruck") === "moreThan12t" &&
           this.valueIs("typeLargeTruck") === "vogntog" &&
           this.valueIs("hej") === "hej"
-        );
+        )
       },
       value: null,
       options: () => {
@@ -232,13 +224,13 @@ export class Model {
           options: ["godkendt", "ikke_godkendt"],
           data: null,
           id: "sammenkobling"
-        };
+        }
       }
     },
     {
       id: "smallTruckType",
       fn: () => {
-        return this.valueIs("weightTruck") === "lessThan12t";
+        return this.valueIs("weightTruck") === "lessThan12t"
       },
       value: null,
       options: () => {
@@ -246,36 +238,36 @@ export class Model {
           options: ["motorkoeretoej", "paahaengskoeretoej"],
           data: null,
           id: "smallTruckType"
-        };
+        }
       }
     },
     {
       id: "akslerRegTruck",
       fn: () => {
-        return this.valuePropIsEither("type", ["truck", "bus"]);
+        return this.valuePropIsEither("type", ["truck", "bus"])
       },
       value: null,
       options: () => {
         const truckType = this.valueIs("weightTruck"),
-          typeLarge = this.valueIs("typeLargeTruck");
+          typeLarge = this.valueIs("typeLargeTruck")
 
-        let mapper;
+        let mapper
 
         if (
           typeLarge === "vogntog" ||
           truckType === "lessThan12t" ||
           this.valueIs("type") === "bus"
         ) {
-          mapper = [2, 3];
+          mapper = [2, 3]
         } else {
-          mapper = [2, 3, 4];
+          mapper = [2, 3, 4]
         }
 
         return {
           options: null,
           data: mapper,
           id: "akslerRegTruck"
-        };
+        }
       }
     },
     {
@@ -284,7 +276,7 @@ export class Model {
         return (
           this.valueIs("weightTruck") === "moreThan12t" &&
           this.valueIs("sammenkobling") === "ikke_godkendt"
-        );
+        )
       },
       value: null,
       options: () => {
@@ -292,13 +284,13 @@ export class Model {
           options: ["1", "2", "3"],
           data: null,
           id: "akslerVogntog"
-        };
+        }
       }
     },
     {
       id: "euroStandard",
       fn: () => {
-        return this.valueIs("weightTruck") === "moreThan12t";
+        return this.valueIs("weightTruck") === "moreThan12t"
       },
       value: null,
       options: () => {
@@ -306,14 +298,14 @@ export class Model {
           options: ["ikkeEuro", "euro1", "euro2"],
           data: null,
           id: "euroStandard"
-        };
+        }
       }
     },
 
     {
       id: "affjedringstype",
       fn: () => {
-        return this.valueIs("weightTruck") === "moreThan12t";
+        return this.valueIs("weightTruck") === "moreThan12t"
       },
       value: null,
       options: () => {
@@ -321,76 +313,76 @@ export class Model {
           options: ["luft", "anden"],
           data: null,
           id: "affjedringstype"
-        };
+        }
       }
     }
-  ];
+  ]
 
-  options = this.getOptions();
+  options = this.getOptions()
 
   constructor(private periods: PeriodsService) {}
 
   getOptions() {
-    return this.parameter.filter(el => {
-      return el.fn();
-    });
+    return this.parameter.filter((el) => {
+      return el.fn()
+    })
   }
 
   allParametersSet() {
-    return this.options.every(el => el.value !== null);
+    return this.options.every((el) => el.value !== null)
   }
 
   getSubPeriods() {
     const type = this.valueIs("type"),
       periodobj = this.periods.getPeriodOptions(type),
-      period = this.valueIs("period");
+      period = this.valueIs("period")
 
-    if (!periodobj || !period) return;
+    if (!periodobj || !period) return
 
-    const obj = periodobj[Number(period) - 1];
+    const obj = periodobj[Number(period) - 1]
 
     if (obj) {
       const periods = [],
         splitDates = obj.split,
         year = splitDates[0].getFullYear(),
-        lastIndex = splitDates.length - 1;
+        lastIndex = splitDates.length - 1
 
       periods.push({
         from: new Date(year, 0, 1),
         to: this.subDate(splitDates[0])
       }),
         splitDates.forEach((el, index) => {
-          const nextElementIsNotTheLast = index + 1 < splitDates.length;
+          const nextElementIsNotTheLast = index + 1 < splitDates.length
 
           if (index === lastIndex) {
-            periods.push({ from: el, to: new Date(year, 11, 31) });
+            periods.push({ from: el, to: new Date(year, 11, 31) })
           } else if (nextElementIsNotTheLast) {
-            periods.push({ from: el, to: this.subDate(splitDates[index + 1]) });
+            periods.push({ from: el, to: this.subDate(splitDates[index + 1]) })
           }
-        });
+        })
 
-      return periods;
+      return periods
     }
 
-    throw Error("periode fejl");
+    throw Error("periode fejl")
   }
 
   isNewRules() {
     return (
       this.valueIs("period") === "1" ||
       (this.valueIs("period") === "2" && this.valueIs("subperiod") === "2")
-    );
+    )
   }
 
   isPrivatBenyttelsesAfgiftVan() {
     const isVan = this.valueIs("type") === "van",
       period = Number(this.valueIs("period")),
-      subPeriod = Number(this.valueIs("subperiod"));
+      subPeriod = Number(this.valueIs("subperiod"))
 
     return (
       (isVan && [1, 2, 3, 4, 5].indexOf(period) > -1) ||
       (period === 6 && subPeriod === 2)
-    );
+    )
   }
 
   getAfgiftsType(id: string) {
@@ -402,28 +394,28 @@ export class Model {
         [1, "*"],
         [2, "*"],
         [3, "*"]
-      ];
+      ]
 
     if (isTaxiOrCar) {
-      data.push([4, 3]);
+      data.push([4, 3])
     } else {
-      data.push([4, 2]);
+      data.push([4, 2])
     }
 
-    const isEjer = !!data.find(el => {
-      return el[0] === period && (el[1] === "*" || subPeriod === el[1]);
-    });
+    const isEjer = !!data.find((el) => {
+      return el[0] === period && (el[1] === "*" || subPeriod === el[1])
+    })
 
     const isOther = !(isTaxiOrCar || isVan),
       confirmedIsEjer = isEjer && !isOther,
-      confirmedIsVaegtAfgift = !isEjer && !isOther;
+      confirmedIsVaegtAfgift = !isEjer && !isOther
 
     if (isOther) {
-      return null;
+      return null
     }
 
     if (this.valueIs("type") === "car" && period === 4 && subPeriod === 2) {
-      return this.valueIs("valgfri") === id;
+      return this.valueIs("valgfri") === id
     }
 
     const txtId = [
@@ -435,42 +427,42 @@ export class Model {
         id: "ejerafgift",
         is: confirmedIsEjer
       }
-    ];
+    ]
 
-    const r = txtId.find(el => el.id === id).is;
+    const r = txtId.find((el) => el.id === id).is
 
-    return r;
+    return r
   }
 
   isVeteranCar() {
     const isCar = this.valueIs("type") === "car",
       period = this.valueIs("period"),
       period_ =
-        period === "7" || (period === "8" && this.valueIs("subperiod") === "1");
+        period === "7" || (period === "8" && this.valueIs("subperiod") === "1")
 
-    return this.valueIs("type") === "veteranbil" || (isCar && period_);
+    return this.valueIs("type") === "veteranbil" || (isCar && period_)
   }
 
   valueIs(id: string) {
-    const obj = this.parameter.find(el => el.id === id);
-    return obj ? obj.value : "";
+    const obj = this.parameter.find((el) => el.id === id)
+    return obj ? obj.value : ""
   }
 
   valuePropIsEither(id: string, values: string[]) {
-    const val = this.valueIs(id);
-    return values.indexOf(val) > -1;
+    const val = this.valueIs(id)
+    return values.indexOf(val) > -1
   }
 
   allMultipleValues(check: general[]) {
-    return check.every(el => {
-      return this.valueIs(el.id) === el.value;
-    });
+    return check.every((el) => {
+      return this.valueIs(el.id) === el.value
+    })
   }
 
   someMultipleValues(check: general[]) {
-    return check.some(el => {
-      return this.valueIs(el.id) === el.value;
-    });
+    return check.some((el) => {
+      return this.valueIs(el.id) === el.value
+    })
   }
 
   subDate(date: Date) {
@@ -478,9 +470,9 @@ export class Model {
       date.getFullYear(),
       date.getMonth(),
       date.getDate() - 1
-    );
+    )
 
-    return newDate;
+    return newDate
   }
 }
 
